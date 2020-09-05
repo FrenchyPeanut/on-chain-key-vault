dc2182d7b515f6a20f24a20cc69962d94b24614d
Charles BEYER <charles.beyer.pro@gmail.com>
2020-09-05 21:07:19 +0200

    feat(cleanup)
    Added documentation for the smart-contract.
    Cleanup some code.

--- packages/buidler/contracts/KeyVault.sol
+++ packages/buidler/contracts/KeyVault.sol
@@ -27,6 +27,12 @@ contract KeyVault {
         initialized = true;
     }
 
+    /**
+     * @dev Initialize the contract for cloning
+     * @param firstOwner name of the secret message to add
+     * @param _sharedKey encrypted message of the secret key with the user derived wallet public-key
+     * @param _salt salt used for signature and HD wallet derivation
+     */
     function initialize(address firstOwner, string memory _sharedKey, string memory _salt) public {
         require(!initialized, 'The contract must not be initialized beforehand.');
         require(firstOwner != address(0), 'Cannot add zero address.');
@@ -37,6 +43,11 @@ contract KeyVault {
         initialized = true;
     }
 
+    /**
+     * @dev Authorize a new user to use the vault
+     * @param _newUserAddress address of the user to whitelist
+     * @param _newUserEncryptedSharedKey encrypted message of the secret key with the user derived wallet public-key
+     */
     function addUserKey(address _newUserAddress, string memory _newUserEncryptedSharedKey) public onlyOwners returns (bool) {
         require(_newUserAddress != address(0), 'Cannot use zero-address.');
         userKeys[_newUserAddress] = _newUserEncryptedSharedKey;
@@ -45,6 +56,10 @@ contract KeyVault {
         return true;
     }
 
+    /**
+     * @dev Remove a user right from the vault
+     * @param _userAddressToRemove address to remove from the vault
+     */
     function removeUser(address _userAddressToRemove) public onlyOwners returns (bool)
     {
         require(_userAddressToRemove != address(0), 'Cannot use zero-address.');
@@ -55,34 +70,64 @@ contract KeyVault {
         return true;
     }
 
+    /**
+     * @dev Set a secret message
+     * @param _secretName name of the secret message to add
+     * @param _encryptedSecret encrypted message to add
+     */
     function setSecret(string memory _secretName, string memory _encryptedSecret) public onlyOwners returns (bool) {
         require(bytes(secretValues[_secretName]).length == 0, 'A secret has already been added.');
         secretValues[_secretName] = _encryptedSecret;
         return true;
     }
 
+    /**
+     * @dev Return a secret message
+     * @param _secretName name of the secret message to retrieve
+     */
     function getSecret(string memory _secretName) public view returns (string memory) {
         return secretValues[_secretName];
     }
 
+    /**
+     * @dev Return the whitelisting status of a specific user
+     * @param _address address of the user to search
+     */
     function getWhitelistedUserStatus(address _address) public view returns (bool) {
         return whitelistedUsers[_address];
     }
 
+    /**
+     * @dev Return the encrypted secret key message of a specific user
+     * @param _address address of the user to search
+     */
     function getUserKeys(address _address) public view returns (string memory) {
         return userKeys[_address];
     }
 
+    /**
+     * @dev Return the salt
+     */
     function getSalt() public view returns (string memory) {
         return salt;
     }
 
+    /**
+     * @dev SafeMath for addition
+     * @param a first parameter
+     * @param b second parameter
+     */
     function add(uint256 a, uint256 b) internal pure returns (uint256){
         require((a + b) >= a, 'SafeMath: Addition overflow');
         uint256 c = a + b;
         return c;
     }
 
+    /**
+     * @dev SafeMath for subtraction
+     * @param a first parameter
+     * @param b second parameter
+     */
     function sub(uint256 a, uint256 b) internal pure returns (uint256){
         require(b <= a, 'SafeMath: Subtraction underflow.');
         uint256 c = a - b;
--- packages/buidler/contracts/KeyVaultFactory.sol
+++ packages/buidler/contracts/KeyVaultFactory.sol
@@ -9,7 +9,7 @@ import '@nomiclabs/buidler/console.sol';
 contract KeyVaultFactory is CloneFactory {
 
     KeyVault[] public keyVaultAddresses;
-    event KeyVaultDeployed(KeyVault metaCoin);
+    event KeyVaultDeployed(KeyVault keyVault_);
 
     address public vaultTarget;
     address private owner;
@@ -26,10 +26,19 @@ contract KeyVaultFactory is CloneFactory {
         owner = msg.sender;
     }
 
+    /**
+     * @dev Set the target contract for cloning
+     * @param _vaultTarget address of the original smart-contract to use for cloning
+     */
     function setLibraryAddress(address _vaultTarget) external onlyOwner {
         vaultTarget = _vaultTarget;
     }
 
+    /**
+     * @dev Create a new user vault
+     * @param _sharedKey encrypted message of the secret key with the user derived wallet public-key
+     * @param _salt salt used for signature and HD wallet derivation
+     */
     function createVault(string calldata _sharedKey, string calldata _salt) external {
         require(!hasVault[msg.sender], 'Cannot deploy another keyVault.');
         KeyVault keyvault = KeyVault(
@@ -43,10 +52,17 @@ contract KeyVaultFactory is CloneFactory {
         hasVault[msg.sender] = true;
     }
 
+    /**
+     * @dev Return the addresses of the user vaults created through the factory
+     */
     function getKeyVaults() external view returns (KeyVault[] memory) {
         return keyVaultAddresses;
     }
 
+    /**
+     * @dev Return the vault address for a specific user
+     * @param _userAddress address of the user to search
+     */
     function getUserKeyVaults(address _userAddress) external view returns (KeyVault) {
         return userVaults[_userAddress];
     }
